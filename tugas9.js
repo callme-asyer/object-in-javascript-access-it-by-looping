ac53d27a9d34fef95eb086c1adea20a8ff0bd515
callme-asyer <53029091+callme-asyer@users.noreply.github.com>
2019-10-08 12:52:03 +0700

    Tugas niomic
    Tugas 9,js

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function namaFilm(){
+ var datafilm= {
+     judul : 'avengers load game',
+     tahun : 2025,
+     actor : 'bring spidey back'
+
+ }
+ for (var x in datafilm){
+   console.log(datafilm[x]);
+ }
+
+}
+namaFilm();
